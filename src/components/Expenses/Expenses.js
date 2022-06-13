5e098dd670b53b71fe3dec73a3f064e6c425457f
Suhas M R <suhasmr96@gmail.com>
2022-06-13 19:01:00 +0530

    Adding public and source code folders

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import ExpensesFilter from "./ExpensesFilter";
+import ExpenseList from "./ExpensesList";
+import Card from "../UI/Card";
+import "./Expenses.css";
+import ExpenseChart from "./ExpensesChart";
+const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2020");
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
+  return (
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+          selectedYear={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        {/* {filteredExpenses.length === 0 ? (
+          <p className="p">"No Expenses Found Here!!!"</p>
+        ) : (
+          filteredExpenses.map((expense) => (
+            <ExpenseItem
+              key={expense.id}
+              title={expense.title}
+              amount={expense.amount}
+              date={expense.date}
+            ></ExpenseItem>
+          ))
+        )} */}
+        <ExpenseChart expenses={filteredExpenses} />
+        <ExpenseList items={filteredExpenses} />
+      </Card>
+    </div>
+  );
+};
+
+export default Expenses;
